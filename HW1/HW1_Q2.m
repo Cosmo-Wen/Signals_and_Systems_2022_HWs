c0e76301fab5cbee22787e1cfeb5b1449f071544
CriticSuika <88042792+CriticSuika@users.noreply.github.com>
2022-03-08 23:55:30 +0800

    Add files via upload

--- HW1/HW1_Q2.m
+++ HW1/HW1_Q2.m
@@ -2,10 +2,10 @@ x = -10 : 10;
 y = 0;
 
 for k = -2 : 2
-    n = k.*(k <= 2 && k >= -2);
-    nshiftk = circshift(x, k);
-    nk = k.*(nshiftk >= -2 & nshiftk <= 2);
-    y = y + nshiftk.*nk;
+    n = k.*(k <= 2 && k >= -2);                 % Determine the original graph
+    nshiftk = circshift(x, k);                  % move the graph n by k
+    nk = k.*(nshiftk >= -2 & nshiftk <= 2);     % Doing the convolution (the amp part)
+    y = y + nshiftk.*nk;                        % Doing the convolution (adding part)
 end
 
 stem(x, y);
