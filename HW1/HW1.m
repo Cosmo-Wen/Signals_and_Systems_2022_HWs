78b4742670452fa422ed9a344baee0c39c388639
Cosmo-Wen <95605718+Cosmo-Wen@users.noreply.github.com>
2022-03-09 10:44:11 +0800

    Combined into HW1.m

--- HW1/HW1.m
+++ HW1/HW1.m
@@ -7,72 +7,65 @@ clc;    clear;
 
 %% Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1Q1
 
-clc; clear; 
-n = linspace(0, 10*pi, 200);
-z1 = exp((1/2)*pi*1i*n); 
-z2 = exp((2/3)*pi*1i*n); 
-z3 = exp((5/6)*pi*1i*n); 
-figure();
+n = 0: 200;
+
+z1 = exp((1/20)*2*pi*1i*n);         % fundemental frequency of 20
+z2 = exp((1/30)*2*pi*1i*n);         % fundemental frequency of 30
+z3 = exp((1/60)*2*pi*1i*n);         % fundemental frequency of 60
+
+figure();                           % figure 1
 stem(n, real(z1), 'o');
 hold on;
 stem(n, real(z2), 'x');
 stem(n, real(z3), '*');
-axis([0 20 -3 3]);
 hold off;
-figure();
-stem(n, real(z1 + z2 + z3), 'h');
-axis([0 20 -3 3]);
+
+figure();                           % figure 2
+stem(n, real(z1 + z2 + z3), 'h');   % fundemental frequency of N
+
+% N = gcd(20, 30, 60), or the first place where z1, z2, z3 intersects
+% same as the conclusion drawn from page 22 of the slides
 
 %% Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2Q2
 
-% using matlab function
 
 clc; clear;
+
 x = -10 : 10;
 y = 0;
 
 for k = -2 : 2
-    nshiftk = circshift(x, k);
-    nk = k.*(nshiftk >= -2 & nshiftk <= 2);
-    y = y + nshiftk.*nk;
+    n = k.*(k <= 2 && k >= -2);                 % Determine the original graph
+    nshiftk = circshift(x, k);                  % move the graph n by k
+    nk = k.*(nshiftk >= -2 & nshiftk <= 2);     % Doing the convolution (the amp part)
+    y = y + nshiftk.*nk;                        % Doing the convolution (adding part)
 end
 
 stem(x, y);
 
-% using formula
+% you don't have to reverse the second function if the values are evaluated from left to right
 
 
 %% Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3Q3
 
-% using matlab function
 
 clc, clear;
+
 t = linspace(-2, 12);
 y = zeros(length(t));
 for li = 1 : length(t)
-    fun = @(x) x.*(t(li)-x).^3;
-    if t(li) < -1
-        y(li) = 0;
-    elseif t(li) < 2
-        y(li) = integral(fun, 0, t(li)+1);
-    elseif t(li) < 3
-        y(li) = integral(fun, t(li)-2, t(li)+1);
-    elseif t(li) < 6
-        y(li) = integral(fun, t(li)-2, 4);
-    else
-        y(li) = 0;
+    fun = @(x) x.*(t(li)-x).^3;                     % f(x) * g(x) = integral(f(t)g(x-t))dx from -inf to inf, fun = f(x)g(t-x)
+    if t(li) <= -1                                  % t + 1 <= 0, t <= -1
+        y(li) = 0;                                  % no overlap
+    elseif t(li) <= 2                               % t + 1 >= 0 and t - 2 <= 0, -1 <= t <= 2
+        y(li) = integral(fun, 0, t(li)+1);          % overlap, integrate fun from 0 to t + 1
+    elseif t(li) <= 3                               % t - 2 >= 0 and t + 1 <= 4, 2 <= t <= 3
+        y(li) = integral(fun, t(li)-2, t(li)+1);    % overlap, integrate fun from t - 2 to t + 1
+    elseif t(li) <= 6                               % t - 2 <= 4, t <= 6
+        y(li) = integral(fun, t(li)-2, 4);          % overlap, intergate fun from t - 2 to 4
+    else                                            % t - 2 >= 4, t >= 6
+        y(li) = 0;                                  % no overlap
     end
 end
+
 plot(t, y);
-
-% using formula
-
-
-
-
-
-
-
-
-
-
--- HW1/HW1_Q1.m
+++ HW1/HW1_Q1.m
@@ -1,14 +1,14 @@
-n = linspace(0, 10*pi, 200);
-z1 = exp((1/2)*pi*1i*n);
-z2 = exp((2/3)*pi*1i*n);
-z3 = exp((5/6)*pi*1i*n);
+n = 0: 200;
+z1 = exp((1/20)*2*pi*1i*n);         % fundemental frequency of 20
+z2 = exp((1/30)*2*pi*1i*n);         % fundemental frequency of 30
+z3 = exp((1/60)*2*pi*1i*n);         % fundemental frequency of 60
 figure();
 stem(n, real(z1), 'o');
 hold on;
 stem(n, real(z2), 'x');
 stem(n, real(z3), '*');
-axis([0 20 -3 3]);
 hold off;
 figure();
-stem(n, real(z1 + z2 + z3), 'h');
-axis([0 20 -3 3]);
+stem(n, real(z1 + z2 + z3), 'h');   % fundemental frequency of N
+
+% N should be gcd(20, 30, 60), or the first place where z1, z2, z3 intersects
