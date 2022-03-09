e8e1bf27e1b2b8bc85094989b8ebc6c75e33acf1
Cosmo-Wen <95605718+Cosmo-Wen@users.noreply.github.com>
2022-03-09 17:11:30 +0800

    Formatted graphs

--- HW1/HW1.m
+++ HW1/HW1.m
@@ -19,10 +19,18 @@ stem(n, real(z1), 'o');
 hold on
 stem(n, real(z2), 'x');
 stem(n, real(z3), '*');
+title('exp of N = 20, 30, 60');
+legend('N = 20', 'N = 30', 'N = 60');
+xlabel('t');
+ylabel('value');
 hold off
 
 subplot(2, 1, 2);
 stem(n, real(z1 + z2 + z3), 'h');   % fundemental frequency of N
+title('Combination of all three');
+legend('gcd(20, 30, 60) = 60');
+xlabel('t');
+ylabel('value');
 
 % N = gcd(20, 30, 60), or the first place where z1, z2, z3 intersects
 % same as the conclusion drawn from page 22 of the slides
@@ -44,6 +52,10 @@ end
 
 figure();
 stem(x, y);
+title('Autoconvolution of x[n]');
+legend('x[n]* x[n]');
+xlabel('n');
+ylabel('value');
 
 % you don't have to reverse the second function if the values are evaluated from left to right
 
@@ -72,6 +84,10 @@ end
 
 figure();
 plot(t, y);
+title('Convolution of x(t) and h(t)');
+legend('x(t) * h(t)');
+xlabel('t');
+ylabel('value');
 
 %% Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4Q4
 
@@ -95,14 +111,29 @@ con = conv(y, delta) / 20000; % discrete convolution with adjustment to dt
 figure();
 subplot(3, 1, 1);
 plot(t, y);
+title('Only h(t)');
+legend('h(t)'); 
+xlabel('t');
+ylabel('value');
 subplot(3, 1, 2);
-plot(tl, con);   
+plot(tl, con);
+title('After convolution'); 
+legend('delta(t) * h(t)');
+xlabel('t');
+ylabel('value');
 axis([0 1 0 1]);
 subplot(3, 1, 3);
-plot(t, y);
+plot(t, y, 'o');
 hold on
-plot(tl, con);
+plot(tl, con, 'x');
+title('Compare between the previous two');
+legend('h(t)', 'delta(t)x * h(t)');
+xlabel('t');
+ylabel('value');
 axis([0 1 0 1]);
 
+% Property 2: Unit Area
 % integral of the delta function yeilds 1000 * 1 / 1000 = 1 (unit area)
+
+% Property 3: Sifting
 % integral of delta * y yeilds 1000 * (1 - exp(1/500)) / 2 = 0.9990006663, approx to 1 = y(0)
