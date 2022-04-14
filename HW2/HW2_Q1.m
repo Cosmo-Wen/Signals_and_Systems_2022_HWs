57d8549747540147778a10b89e8bea593b67fce2
Cosmo-Wen <95605718+Cosmo-Wen@users.noreply.github.com>
2022-04-14 21:23:42 +0800

    Completed Q1 with comments and graph illustration.

--- HW2/HW2_Q1.m
+++ HW2/HW2_Q1.m
@@ -1,19 +1,63 @@
 clc; clear;
 
-% exp(-(t/t0)^2) = t0 * sqrt(pi) * exp(-w^2 * t^2 / 4)
+% exp(-(t/t0)^2) = t0 * sqrt(pi) * exp(-ω^2 * t^2 / 4)
 
-Fs = 1000;
-t1 = (-50: 1 / Fs: 50) * 10^-12;
-t2 = (-5: 1 / Fs: 5) * 10^-12;
-x1 = exp(-t1.^2 / (30*10^-12)^2);
-x2 = exp(-t2.^2 / (10^-12)^2);
-X1 = fftshift(abs(fft(x1)));
-X2 = fftshift(abs(fft(x2)));
-subplot(2, 2, 1)
+% Definitions
+pico = 10^-12;  % define pico 
+
+Fs = 500000;    % sampling frequency
+
+% Constructing Pulses
+t1 = (-50: 1/Fs: 50 - 1 / Fs) * pico; 
+t2 = (-5: 1/Fs: 5 - 1 / Fs) * pico;
+x1 = exp(-t1.^2 / (30 * pico)^2);   % Gaussian pulse of t0 = 30ps
+x2 = exp(-t2.^2 / (1 * pico)^2);    % Gaussian pulse of t0 = 1ps
+
+% Constructing Frequency Scale
+df1 = Fs * pico / length(x1);   % frequency difference divided evenly regarding t1
+df2 = Fs * pico  / length(x2);  % frequency difference divided evenly regarding t2
+freq1 = -Fs * pico  / 2: df1: Fs * pico / 2 - df1;  % set the scale to [-Fs / 2: Fs / 2) with regards to df1
+freq2 = -Fs * pico / 2: df2: Fs * pico / 2 - df2;   % set the scale to [-Fs / 2: Fs / 2) with regards to df2
+
+% Fourier Transformation
+%   For Both Instances:
+%       1. Perform fast fourier transform
+%       2. Mirror 2pi(f) to (-f), and shift by fftshift
+%       3. Adjust for scale of integral
+X1 = fftshift(abs(fft(x1))) / Fs * pico;    
+X2 = fftshift(abs(fft(x2))) / Fs * pico;
+
+% Plotting
+% Plot 1: t0 = 30ps
+subplot(2, 2, 1);
 plot(t1, x1);
-subplot(2, 2, 2)
-plot(t2, x2)
-subplot(2, 2, 3)
-plot(X1)
-subplot(2, 2, 4)
-plot(X2)
+title('x1(t0 = 30ps)');
+legend('x1(t)'); 
+xlabel('t (s)');
+ylabel('x1(t)');
+
+% Plot 2: t0 = 1ps
+subplot(2, 2, 2);
+plot(t2, x2);
+title('x2(t0 = 1ps)');
+legend('x2(t)'); 
+xlabel('t (s)');
+ylabel('x2(t)');
+
+% Plot 3: Fourier Transform of x1(t)
+subplot(2, 2, 3);
+plot(freq1, X1);
+axis([(-.03 * pico) (.03 * pico) 0 (75 * pico)]);
+title('Fourier Transform of x1(t): X1(jω)');
+legend('X1(jω)'); 
+xlabel('ω (rad/s)');
+ylabel('Magnitude of X1(jω)');
+
+% Plot 4: Fourier Transform of x2(t)
+subplot(2, 2, 4);
+plot(freq2, X2);
+axis([(-1 * pico) (1 * pico) 0 (2 * pico)]);
+title('Fourier Transform of x2(t): X2(jω)');
+legend('X2(jω)'); 
+xlabel('ω (rad/s)');
+ylabel('Magnitude of X2(jω)');
