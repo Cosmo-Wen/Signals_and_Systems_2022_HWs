beb6bff4c75de6d65063f029ceb622c5023c05ae
Cosmo-Wen <95605718+Cosmo-Wen@users.noreply.github.com>
2022-04-20 21:37:26 +0800

    Fixed marginal error

--- HW2/HW2_Q4.m
+++ HW2/HW2_Q4.m
@@ -22,26 +22,43 @@ G = fftshift(abs(fft(g))) / Fs * micro;     % Fourier transform of g(t)
 P = fftshift(abs(fft(p))) / Fs * micro;     % Fourier transform of p(t)
 R1 = fftshift(abs(fft(r))) / Fs * micro;    % Fourier transform of r(t)
 
-freq4 = (-Fs + df1: df1: Fs - df1) * 2 * pi / micro;    % set the scale to [-Fs : Fs] because of convolution
 R2 = (conv(G, P) * (df1 * 2 * pi / micro)) / (2*pi);    % Convolution of G, P and divided the result by 2*pi
+freq4 = (-Fs: df1: Fs - df1) * 2 * pi / micro;    % set the scale to [-Fs : Fs) because of convolution
+dif = (length(freq4) - length(freq3)) / 2;  % calculate margin
+R2 = R2(dif + 1: length(freq4) - dif);  % parse margin of R2
+freq4= freq4(dif + 1: length(freq4) - dif); % parse margin of freq4
+
 % Because we use DT convolution to calculate CT convolution
-% so we should multiply the result by the interval of the DT signals
+% We should multiply the result by the interval of the DT signals
 
 % R1 is obtained by fourier transform of r(t)
 % R2 is obtained by multiply 1/2*pi to the convolution of G(jw) and P(jw)
 % plot both R1 and R2 to prove that their results are same
-%Plot1: fourier transform of r(t)
-subplot(1, 2, 1);
-plot(freq3, R1);
-axis([-100 / micro, 100 / micro, 0, 2 * micro]);
-title('R1: F{r(t)}');
-xlabel('w');
-ylabel('R1(jw)');
 
-%Plot2: 1/(2*pi) * conv(G(jw), P(jw))
-subplot(1, 2, 2);
+% Plot 1: r(t)
+subplot(3, 1, 1);
+plot(t, r);
+title('r(t)');
+legend('r(t)');
+xlabel('t (s)');
+ylabel('r(t)');
+
+% Plot 2: fourier transform of r(t)
+subplot(3, 1, 2);
+plot(freq3, R1);
+hold on;
 plot(freq4, R2);
-axis([-100 / micro, 100 / micro, 0, 2 * micro]);
-title('R2: 1/(2*pi) * conv(G(jw), P(jw))');
-xlabel('w');
-ylabel('R2(jw)');
+hold off;
+axis([-100 / micro, 100 / micro, 0, 1 * micro]);
+title('R1: Fourier Transform of r(t)');
+legend('R1', 'R2');
+xlabel('ω (rad/s)');
+ylabel('Magnitude');
+
+% Plot 3: 1/(2*pi) * conv(G(jw), P(jw))
+subplot(3, 1, 3);
+plot(freq4, R2 - R1);
+axis([-100 / micro, 100 / micro, 0, 1 * micro]);
+title('Difference between R1 R2');
+xlabel('ω (rad/s)');
+ylabel('Magnitude');
