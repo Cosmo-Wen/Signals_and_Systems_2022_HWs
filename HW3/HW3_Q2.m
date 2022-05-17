249e66085ba7994ea3df97be40b826a3141f3f9c
Cosmo-Wen <95605718+Cosmo-Wen@users.noreply.github.com>
2022-05-17 21:19:08 +0800

    Comments and Bug Fixes for Q2 and Q3

--- HW3/HW3_Q2.m
+++ HW3/HW3_Q2.m
@@ -1,35 +1,90 @@
 clc;    clear;
 
+% Sampling Frequency
 Fs = 5000;
 
-wr = 2 * pi * 10^9;
-t = -5: 1 / Fs: 5;
+% Δω = 2π * 10^9
+dw = 2 * pi * 10^9;
 
+% Set time and frequency scale
+t = -5: 1 / Fs: 5;
+df = Fs / length(t);
+freq = (-Fs / 2: df: Fs / 2 - df) * 2 * pi;
+
+% β = 2π
 b1 = 2 * pi;
-e1 = exp(1i * b1 * sin(wr * t));
+
+% Construct e1 and its Fourier Transform
+e1 = exp(1i * b1 * sin(dw * t));
 E1 = fftshift(fft(fftshift(e1))) / Fs;
 
+% β = 4π
 b2 = 4 * pi;
-e2 = exp(1i * b2 * sin(wr * t));
+
+% Construct e2 and its Fourier Transform
+e2 = exp(1i * b2 * sin(dw * t));
 E2 = fftshift(fft(fftshift(e2))) / Fs;
 
-E3 = abs(E1) .* exp(1i * angle(E1));
-E4 = abs(E2) .* exp(1i * angle(E2));
-
+% E3 = Magnitude of E1 and Phase of 1
+E3 = abs(E1) .* exp(1i);
 e3 = ifftshift(ifft(ifftshift(E3 * Fs)));
+
+%  E4 = Magnitude of E2 and Phase of 1
+E4 = abs(E2) .* exp(1i);
 e4 = ifftshift(ifft(ifftshift(E4 * Fs)));
 
+% Plot |e1|^2
 subplot(3, 3, 1);
 plot(t, abs(e1).^2);
+title('|e1(t)|^2');
+legend('|e1(t)|^2'); 
+xlabel('t (s)');
+ylabel('e1(t)');
+
+% Plot Magnitude of E1
 subplot(3, 3, 2);
 plot(freq, abs(E1));
+title('|E1(jw)|');
+legend('|E1(jw)|'); 
+xlabel('w (rad)');
+ylabel('E1(jw)');
+
+% Plot Phase of E1
 subplot(3, 3, 3);
 plot(freq, angle(E1));
+title('∠E1(jw)');
+legend('∠E1(jw)∠'); 
+xlabel('w (rad)');
+ylabel('ϕ');
+
+% Plot Magnitude of E2
 subplot(3, 3, 4);
 plot(freq, abs(E2));
+title('|E2(jw)|');
+legend('|E2(jw)|'); 
+xlabel('w (rad)');
+ylabel('E2(jw)');
+
+% Plot Phase of E2
 subplot(3, 3, 5);
 plot(freq, angle(E2));
+title('∠E2(jw)');
+legend('∠E2(jw)∠'); 
+xlabel('w (rad)');
+ylabel('ϕ');
+
+% Plot |e3|^2
 subplot(3, 3, 7);
 plot(freq, abs(e3).^2);
+title('|e3(t)|^2');
+legend('|e3(t)|^2'); 
+xlabel('t (s)');
+ylabel('e3(t)');
+
+% Plot |e4|^2
 subplot(3, 3, 8);
 plot(freq, abs(e4).^2);
+title('|e4(t)|^2');
+legend('|e4(t)|^2'); 
+xlabel('t (s)');
+ylabel('e4(t)');
--- HW3/HW3_Q3.m
+++ HW3/HW3_Q3.m
@@ -1,41 +1,104 @@
 clc;    clear;
 
+% Sampling Frequency
 Fs = 50000;
+
+% Constructing time and frequency scale
 t = 0: 1 / Fs: 0.1 - 1 / Fs;
 df = Fs / length(t);
 w = (-Fs / 2: df: Fs / 2 - df) * 2 * pi;
 
+% Second Order Differential System
 % y''(t) + 2ζω0 y'(t) + ω0^2 y(t) = ω0^2 x(t)
 % (-ω^2 + 2ζω0(jω) + ω0^2) Y(jω) = ω0^2 X(jω)
 % H(jω) = (ω0^2) / (-ω^2 + 2ζω0(jω) + ω0^2)
 
+% ωn
 wn = 100;
+
+% Different ζ's
 z1 = 0.5;
 z2 = 1;
 z3 = 1.5;
+
+% Construct Fourier Transform for Impulse Responses
 H1 = wn^2 ./ (-w.^2 + 2 * z1 * wn * 1i * w + wn^2);
 H2 = wn^2 ./ (-w.^2 + 2 * z2 * wn * 1i * w + wn^2);
 H3 = wn^2 ./ (-w.^2 + 2 * z3 * wn * 1i * w + wn^2);
 
+% Impulse Responses by Inverse Fourier Transform
 h1 = ifft(ifftshift(H1));
 h2 = ifft(ifftshift(H2));
 h3 = ifft(ifftshift(H3));
 
+% Plot Impulse Response of ζ = 0.5
 subplot(3, 3, 1);
 plot(t, h1);
+title('Impulse Response h1(t)');
+legend('h1(t)'); 
+xlabel('t (s)');
+ylabel('h1(t)');
+
+% Plot Bode Magnitude of ζ = 0.5
 subplot(3, 3, 2);
-semilogx(w, 20 * log(abs(H1)))
+semilogx(w, 20 * log(abs(H1)));
+title('Bode Plot Magnitude H1(jw)');
+legend('|H1(jw)|'); 
+xlabel('w (rad)');
+ylabel('|H1(t)|');
+
+% Plot Bode Phase of ζ = 0.5
 subplot(3, 3, 3);
-semilogx(w, angle(H1))
+semilogx(w, angle(H1));
+title('Bode Plot Phase H1(jw)');
+legend('∠H1(jw)'); 
+xlabel('w (rad)');
+ylabel('∠H1(t)');
+
+% Plot Impulse Response of ζ = 0.5
 subplot(3, 3, 4);
 plot(t, h2);
+title('Impulse Response h2(t)');
+legend('h2(t)'); 
+xlabel('t (s)');
+ylabel('h2(t)');
+
+% Plot Bode Magnitude of ζ = 0.5
 subplot(3, 3, 5);
-semilogx(w, 20 * log(abs(H2)))
+semilogx(w, 20 * log(abs(H2)));
+title('Bode Plot Magnitude H2(jw)');
+legend('|H2(jw)|'); 
+xlabel('w (rad)');
+ylabel('|H2(t)|');
+
+% Plot Bode Phase of ζ = 0.5
 subplot(3, 3, 6);
-semilogx(w, angle(H2))
+semilogx(w, angle(H2));
+title('Bode Plot Phase H2(jw)');
+legend('∠H2(jw)'); 
+xlabel('w (rad)');
+ylabel('∠H2(t)');
+
+% Plot Impulse Response of ζ = 0.5
 subplot(3, 3, 7);
 plot(t, h3);
+title('Impulse Response h3(t)');
+legend('h3(t)'); 
+xlabel('t (s)');
+ylabel('h3(t)');
+
+% Plot Bode Magnitude of ζ = 0.5
 subplot(3, 3, 8);
-semilogx(w, 20 * log(abs(H3)))
+semilogx(w, 20 * log(abs(H3)));
+title('Bode Plot Magnitude H3(jw)');
+legend('|H3(jw)|'); 
+xlabel('w (rad)');
+ylabel('|H3(t)|');
+
+% Plot Bode Phase of ζ = 0.5
 subplot(3, 3, 9);
-semilogx(w, angle(H3))
+semilogx(w, angle(H3));
+title('Bode Plot Phase H3(jw)');
+legend('∠H3(jw)'); 
+xlabel('w (rad)');
+ylabel('∠H3(t)');
