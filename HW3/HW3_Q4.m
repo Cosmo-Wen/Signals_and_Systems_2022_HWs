3c2cf44c13ec0360277909773bbb33d41b0d9c41
CriticSuika <88042792+CriticSuika@users.noreply.github.com>
2022-05-17 22:50:09 +0800

    dumb dumb 0.25 to 0.5

--- HW3/HW3_Q4.m
+++ HW3/HW3_Q4.m
@@ -9,12 +9,12 @@ w = -2*pi:dw:2*pi;
 % define the range of w
 
 h_0 = (n+1).*0.5.^n .* (n>=0);
-h_25 = 0.5.^n.*(sin((n+1)*0.25*pi)/sin(0.25*pi)) .* (n>=0);
+h_5 = 0.5.^n.*(sin((n+1)*0.5*pi)/sin(0.5*pi)) .* (n>=0);
 h_pi = (n+1).*(-0.5).^n .* (n>=0);
 % according to Eqn.(6.66), the impulse response h[n] = (n + 1)((-r)^n)u[n]
 
 H_0 = 1./(1-2*0.5*cos(0).*exp(-1j.*w)+0.5^2.*exp(-2j.*w));
-H_25 = 1./(1-2*0.5*cos(0.25*pi).*exp(-1j.*w)+0.5^2.*exp(-2j.*w));
+H_5 = 1./(1-2*0.5*cos(0.5*pi).*exp(-1j.*w)+0.5^2.*exp(-2j.*w));
 H_pi = 1./(1-2*0.5*cos(pi).*exp(-1j.*w)+0.5^2.*exp(-2j.*w));
 % according to Eqn.(6.58), the frequency response H[n] = 1/(1 - 2rcosθ*e^(-jw) + r^2*e^(-2jw))
 
@@ -35,13 +35,13 @@ xlabel('w');
 ylabel('20log|H(e^j^w)|');
 
 subplot(3,2,3)
-stem(n,h_25)
+stem(n,h_5)
 title('Impulse Response When θ = 0.5pi'); 
 xlabel('n');
 ylabel('h[n]');
 
 subplot(3,2,4)
-plot(w,20*log10(H_25))
+plot(w,20*log10(H_5))
 set(gca,'XTick',-2*pi:pi:2*pi)
 set(gca,'XTickLabel',{'-2\pi','-\pi','0','\pi','2\pi'})
 title('Bode Plots for θ = 0.5pi'); 
