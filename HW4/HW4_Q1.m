c921f9d7d34a9ab7b5f656d55e46426527190b71
Cosmo-Wen <95605718+Cosmo-Wen@users.noreply.github.com>
2022-06-09 19:57:19 +0800

    Working Q1 Updated

--- HW4/HW4_Q1.m
+++ HW4/HW4_Q1.m
@@ -1,35 +1,29 @@
-info = audioinfo('Sound_clip_2022.wav'); %Gain info of audio data
-[audio,Fs] = audioread('Sound_clip_2022.wav'); %Read the data of audio
-audiolength = length(audio); %Gain the length of audio
+clc; clear;
 
-%Set up sampling numbers
+% Read audio file
+info = audioinfo('Sound_clip_2022.wav'); % Gain info of audio data
+[audio,Fs] = audioread('Sound_clip_2022.wav'); % Read the data of audio
+audiolength = length(audio); % Gain the length of audio
+
+% Set up sampling numbers
 t = 1:1:audiolength;
-T = t/48000; %Since the value gained is 48000
 
-%Checking (Needed to be deleted when handed)
-figure(1),
-plot(T,audio(1:audiolength));
-xlabel('Time');
-ylabel('Audio Signal');
-title('Original Audio File Signal in Time Domain');
+% Performing Fourier Transform
+FFT = fftshift(fft(fftshift(audio)));
+F = (-Fs / 2: Fs / length(t): Fs / 2 - Fs / length(t)) * 2 * pi;
+F = transpose(F);
 
-%Performing Fourier Transform
-T = transpose(T);
-FFT = fft(audio);
-FFT_p = fft(audio_pro);
-f= 0:(Fs/audiolength):Fs-(Fs/audiolength);    
+% Find specific bandwidths and intensities to use
+sample_piano = FFT.*(abs(F) < 2500) .* (abs(FFT) > 1000);
+sample_violin = FFT.*(abs(F) > 18000) .* (abs(FFT) > 50);
+sample_prof = FFT .* (abs(F) > 1200 & abs(F) < 5000) .* (abs(FFT) < 1000);
 
+% Inverse Fourier Transform
+audio_piano = ifftshift(ifft(ifftshift(sample_piano)));
+audio_violin = ifftshift(ifft(ifftshift(sample_violin)));
+audio_prof = ifftshift(ifft(ifftshift(sample_prof))) * 1.75;
 
-F = transpose(f);
-
-sample_piano = FFT.*(F<400|F>47600);
-sample_violin = FFT.*(F<44000&F>4000);
-sample_prof = FFT.*(F<800&F>200);
-
-audio_piano = ifft(sample_piano);
-audio_violin = ifft(sample_violin);
-audio_prof = ifft(sample_prof);
-
+% Write audio files
 audiowrite('piano.wav', audio_piano, Fs);
-audiowrite('prof.wav', audio_prof, Fs);
 audiowrite('violin.wav', audio_violin, Fs);
+audiowrite('prof.wav', audio_prof, Fs);
